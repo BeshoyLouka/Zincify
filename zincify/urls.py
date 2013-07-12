c406d479f81abac7f4f061d84f5bd232e00e55f1
Beshoy Louka <beshoy.louka@gmail.com>
2013-07-12 14:06:54 -0400

    adding the main django project dir

---
+++ zincify/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'fronts.views.home', name='home'),
+    # url(r'^zincify/', include('zincify.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += static(settings.MEDIA_URL,
+	document_root=settings.MEDIA_ROOT)

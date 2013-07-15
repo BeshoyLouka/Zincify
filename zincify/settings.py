aef80af69f1261025b2174dbab2b2c8d5a3823ec
Beshoy Louka <beshoy.louka@gmail.com>
2013-07-15 12:51:31 -0400

    the right settings for serving static app

--- zincify/settings.py
+++ zincify/settings.py
@@ -69,14 +69,14 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, '../static'),
 )
 
 # List of finder classes that know how to find static files in

3c7234bea86eff45f39872f872145a558a957742
Beshoy Louka <beshoy.louka@gmail.com>
2013-07-16 14:34:34 -0400

    adding the fonts dir to be copied on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,6 +245,7 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,txt}',
                         '.htaccess',
+                        'fonts',
                         'images/{,*/}*.{webp,gif}'
                     ]
                 }]
